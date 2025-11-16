f79884362c77f32571ea1bb6764dc83b5ee048de
Sushant <sushant.barkate@ityx.de>
2025-11-16 19:28:52 +0530

    changes

--- src/main/java/com/contact/service/security/SecurityConfig.java
+++ src/main/java/com/contact/service/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://customer-manoment.netlify.app")); // your Netlify app URL
+        config.setAllowedOrigins(List.of("https://customer-manoment.netlify.app", "http://localhost:8081")); // your Netlify app URL
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
